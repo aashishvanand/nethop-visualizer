be19f5ea4eee56b8d062c213a4d94f498c517d49
Aashish Vivekanand <aashishvanand@gmail.com>
2024-07-18 14:47:40 +0800

    Fixed issued with map

--- src/components/map.js
+++ src/components/map.js
@@ -3,13 +3,18 @@
 import { MapContainer, TileLayer, Polyline } from 'react-leaflet';
 
 export default function Map({ coords }) {
+  const parsedCoords = coords.map(coord => {
+    const [lat, lon] = coord.loc;
+    return [parseFloat(lat), parseFloat(lon)];
+  });
+
   return (
     <MapContainer center={[0, 0]} zoom={2} style={{ height: '500px', width: '100%', marginTop: '1rem' }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      <Polyline positions={coords.map(coord => [parseFloat(coord[0]), parseFloat(coord[1])])} color="red" />
+      <Polyline positions={parsedCoords} color="red" />
     </MapContainer>
   );
 }
